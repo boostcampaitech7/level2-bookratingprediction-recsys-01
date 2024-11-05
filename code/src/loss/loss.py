631076ba38b2453558936715d3c23fed43025497
jinnk0 <jin8073@gmail.com>
2024-11-05 11:15:09 +0900

    feat: add CVAE loss function

---
+++ code/src/loss/loss.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from torch.nn import MSELoss, L1Loss as MAELoss
+from torch.nn import *
+
+class RMSELoss(nn.Module):
+    def __init__(self):
+        super(RMSELoss, self).__init__()
+        self.eps = 1e-6
+    def forward(self, x, y):
+        criterion = MSELoss()
+        loss = torch.sqrt(criterion(x, y)+self.eps)
+        return loss
+    
+class MSE_KLDLoss(nn.Module):
+    def __init__(self, mu, log_var):
+        super(MSE_KLDLoss, self).__init__()
+        self.mu = mu
+        self.log_var = log_var
+    def forward(self, x, y):
+        MSE = nn.functional.mse_loss(x, y, reduction='sum')
+        KLD = -0.5 * torch.sum(1 + self.log_var - self.mu.pow(2) - self.log_var.exp())
+        return MSE + KLD
